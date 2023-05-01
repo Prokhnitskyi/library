16f731a8794c3d151952f6855c874d858c9f45ba
andrii.prokhnitskyi <26540483+Prokhnitskyi@users.noreply.github.com>
2023-05-01 07:18:29 +0300

    Change index to UUID in datasets

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ const booksContainer = document.querySelector('.books-container');
 const cardBlueprint = document.querySelector('#card-template');
 
 function Book({title = '', author = '', pages = 0, read = false}) {
+    this.id = uuidv4();
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -27,16 +28,22 @@ function addBookToLibrary(event) {
 
 function populate(elements = myLibrary) {
     booksContainer.innerHTML = '';
-    elements.forEach((element, index) => {
+    elements.forEach((element) => {
         const card = cardBlueprint.content.cloneNode(true);
-        card.querySelector('.book__read-switch').dataset.index = index.toString();
-        card.querySelector('.book__remove').dataset.index = index.toString();
-        card.querySelector('.book__title').textContent = element.title;
-        card.querySelector('.book__author').textContent = element.author;
-        card.querySelector('.book__pages').textContent = element.pages;
-        card.querySelector('.book__read').textContent = element.read;
+        card.querySelector('.book__read-switch').dataset.index = element.id;
+        card.querySelector('.book__remove').dataset.index = element.id;
+        card.querySelector('.book__title').textContent += element.title;
+        card.querySelector('.book__author').textContent += element.author;
+        card.querySelector('.book__pages').textContent += element.pages;
+        card.querySelector('.book__read').textContent += element.read;
         booksContainer.append(card);
     });
+}
+
+function uuidv4() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
 }
 
 addBookButton.addEventListener('click', () => modal.showModal());
